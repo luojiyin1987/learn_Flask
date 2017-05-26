ebc2443a89677839dfb5f29fb2c2738522dbdec2
luojiyin <luojiyin@hotmail.com>
2017-05-26 00:07:14 -0700

    you need to use   template

--- run.py
+++ run.py
@@ -2,4 +2,23 @@
 # coding=utf-8
 
 from app import app
-app.run(host='0.0.0.0', debug=True)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    return '''
+              <html>
+                <head>
+                    <title>Home Page</title>
+                </head>
+                <body>
+                    <h1>Hello, ''' + user['nickname'] + '''</h1>
+                </body>
+              </html>
+              '''
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
